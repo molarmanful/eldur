f8aaa6bff8d45e712fb80a563c77888ecb8b9a77
molarmanful <benjinpang@gmail.com>
2023-12-03 12:22:20 -0500

    tweaks

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-mkdir -p deps
+mkdir -p deps out
 
 [ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar "https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar"
 
-mkdir -p out/{ttf,otb,bdf}
 for ext in ttf otb bdf; do
 	java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "out/eldur.$ext" ./src/eldur.kbitx
 done
