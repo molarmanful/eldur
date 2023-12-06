92d596c9f309378d47fe3b57f4b59f8108217307
molarmanful <benjinpang@gmail.com>
2023-12-06 16:50:37 -0500

    test

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
+<div align="center">
+
 # ![eldur](./img/eldur.png)
 
 > A stylish bitmap microfont for all your spellcasting needs.
+
+</div>
+
+## Overview
+
+**eldur** is a monospace bitmap font with a 5x13 bounding box (4px avg width,
+5px x-height).
+
+## Installation
+
+Download from [Releases](https://github.com/molarmanful/eldur/releases).
+Included are bitmap formats - bdf, otb, dfont (Mac) - as well as ttf. 2x
+versions are available for HiDPI screens.
+
+For the crispiest viewing experience, try to use the bitmap formats when
+possible. If bitmap fonts are not supported (e.g. in VSCode), then use the TTF
+at font sizes that are multiples of 13px.
+
+> **Quick Tip**: If you need font size in pt, use the following conversion:
+>
+> `pt = px - 72 / dpi`
+>
+> For example: 13px on a 96dpi screen is `13px * 72 / 96dpi = 9.75pt`.
+
+## Contributing
+
+Issues, feature/glyph requests, and pull requests are all welcome!
+
+## Credits
+
+- [Bits'n'Picas](https://github.com/kreativekorp/bitsnpicas)
--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash -e
 
 rm -rf out
 mkdir -p deps out
 
 [ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar "https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar"
 
-exts=(ttf otb dfont psf)
+exts=(ttf otb dfont)
 
 java -jar deps/BitsNPicas.jar convertbitmap -f bdf -o ./out/eldur.bdf ./src/eldur.kbitx
 bdfresize -f 2 ./out/eldur.bdf >./out/eldur_2x.bdf
@@ -13,6 +13,10 @@ bdfresize -f 2 ./out/eldur.bdf >./out/eldur_2x.bdf
 for ext in "${exts[@]}"; do
 	out="out/eldur.$ext"
 	out2x="out/eldur_2x.$ext"
-	java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out" ./out/eldur.bdf
-	java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out2x" ./out/eldur_2x.bdf
+	fontforge -lang=ff -c 'Open($1); Generate($2)' ./out/eldur.bdf "$out"
+	fontforge -lang=ff -c 'Open($1); Generate($2)' ./out/eldur_2x.bdf "$out2x"
+	# java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out" ./out/eldur.bdf
+	# java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out2x" ./out/eldur_2x.bdf
 done
+
+rm -f ./out/*.afm
