7ddbd84e24e59d08edaf6cb06ae962820411c74a
molarmanful <benjinpang@gmail.com>
2023-12-06 05:08:18 -0500

    all kinds of stuff

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# eldur
+# ![eldur](./img/eldur.png)
 
 > A stylish bitmap microfont for all your spellcasting needs.
--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+rm -rf out
 mkdir -p deps out
 
 [ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar "https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar"
 
-for ext in ttf otb bdf; do
-    java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "out/eldur.$ext" ./src/eldur.kbitx
+exts=(ttf otb dfont psf)
+
+java -jar deps/BitsNPicas.jar convertbitmap -f bdf -o ./out/eldur.bdf ./src/eldur.kbitx
+bdfresize -f 2 ./out/eldur.bdf >./out/eldur_2x.bdf
+
+for ext in "${exts[@]}"; do
+	out="out/eldur.$ext"
+	out2x="out/eldur_2x.$ext"
+	java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out" ./out/eldur.bdf
+	java -jar deps/BitsNPicas.jar convertbitmap -f "$ext" -o "$out2x" ./out/eldur_2x.bdf
 done
