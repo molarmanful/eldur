130fe55aefb1b58cf70994296e6ff617ef2d1710
molarmanful <benjinpang@gmail.com>
2024-02-01 11:56:57 -0500

    versioned release artifacts

--- build.sh
+++ build.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -e
 
+while getopts ":v:" o; do
+	case $o in
+	v)
+		[ "$OPTARG" != "" ] && v="_$OPTARG"
+		;;
+	*) ;;
+	esac
+done
+
 rm -rf out
 mkdir -p deps out
 
 [ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar "https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar"
 
-# kbitx to bdf
-java -jar deps/BitsNPicas.jar convertbitmap -f bdf -o ./out/eldur.bdf ./src/eldur.kbitx
-bdfresize -f 2 ./out/eldur.bdf >./out/eldur_2x.bdf
+cp LICENSE out
 
-# kbitx to otb
-java -jar deps/BitsNPicas.jar convertbitmap -f otb -o ./out/eldur.otb ./src/eldur.kbitx
-bdfresize -f 2 ./out/eldur.bdf >./out/eldur_2x.otb
+# kbitx -> bdf
+java -jar deps/BitsNPicas.jar convertbitmap -f bdf -o "out/eldur$v.bdf" src/eldur.kbitx
+bdfresize -f 2 "out/eldur$v.bdf" >"out/eldur_2x$v.bdf"
 
-# kbitx to ttf
-java -jar deps/BitsNPicas.jar convertbitmap -f ttf -o ./out/eldur.ttf ./src/eldur.kbitx
+# kbitx -> otb
+java -jar deps/BitsNPicas.jar convertbitmap -f otb -o "out/eldur$v.otb" src/eldur.kbitx
+bdfresize -f 2 "out/eldur$v.bdf" >"out/eldur_2x$v.otb"
 
-rm -f ./out/*.afm
+# kbitx -> ttf
+java -jar deps/BitsNPicas.jar convertbitmap -f ttf -o "out/eldur$v.ttf" src/eldur.kbitx
+
+rm -f out/*.afm
+zip -r "out/eldur$v.zip" out/*
