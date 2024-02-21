5fb1f65bb732b46e243bc0e7ee08ec9e9421166e
molarmanful <benjinpang@gmail.com>
2024-02-21 17:52:03 -0500

    remove fontforge? (fix too wide width)

--- build.sh
+++ build.sh
@@ -14,30 +14,23 @@ done
 rm -rf out
 mkdir -p deps out
 
-[ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar "https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar"
+[ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar
 
 cp LICENSE out
 cp README.md out
-
-ff() {
-	fontforge -script scripts/fix.py "$@"
-}
 
 bnp() {
 	java -jar deps/BitsNPicas.jar convertbitmap -f "$3" -o out/"$2.$3" "$1"
 }
 
 bnp src/eldur.kbitx eldur ttf
-ff eldur ttf
 bnp src/eldur.kbitx eldur bdf
 sed -i -e '/^FONT/s/-[pc]-/-M-/i' -e '/^FONT/s/-80-/-50-/' out/eldur.bdf
-ff eldur bdf
-ff eldur otb 1
+bnp src/eldur.kbitx eldur otb
 
 bdfresize -f 2 out/eldur.bdf >out/eldur2x.bdf
-sed -i 's/^iso.*-FONT/FONT/g' out/eldur2x.bdf
-ff eldur2x bdf
-ff eldur2x otb 1
+sed -i -e 's/^iso.*-FONT/FONT/g' -e 's/eldur/eldur2x/g' out/eldur2x.bdf
+bnp out/eldur2x.bdf eldur2x otb
 
 rm -f out/*-*.bdf
 
