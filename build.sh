a17f196d09f3e4e36e368222910599cd41e91b89
molarmanful <benjinpang@gmail.com>
2024-02-22 01:01:27 -0500

    fontforge-based fix

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ rm -rf out
 mkdir -p deps out
 
 [ ! -f deps/BitsNPicas.jar ] && wget -O deps/BitsNPicas.jar https://github.com/kreativekorp/bitsnpicas/releases/latest/download/BitsNPicas.jar
+[ ! -f deps/fontforge ] && wget -O deps/fontforge https://github.com/fontforge/fontforge/releases/download/20230101/FontForge-2023-01-01-a1dad3e-x86_64.AppImage
 
 cp LICENSE out
 cp README.md out
@@ -23,14 +24,28 @@ bnp() {
 	java -jar deps/BitsNPicas.jar convertbitmap -f "$3" -o out/"$2.$3" "$1"
 }
 
+ff() {
+	s=$(
+		cat <<-END
+			f = open(argv[1])
+			f.encoding = "UnicodeFull"
+			f.fullname = argv[3]
+			f.fontname = argv[3]
+			f.generate(argv[2], "otb")
+			f.generate(argv[2] + "dfont", "sbit")
+		END
+	)
+	deps/fontforge -c "$s" "$PWD"/out/"$1".bdf "$PWD"/out/"$1". "$1"
+}
+
 bnp src/eldur.kbitx eldur ttf
 bnp src/eldur.kbitx eldur bdf
 sed -i -e '/^FONT/s/-[pc]-/-M-/i' -e '/^FONT/s/-80-/-50-/' out/eldur.bdf
-bnp src/eldur.kbitx eldur otb
+ff eldur
 
 bdfresize -f 2 out/eldur.bdf >out/eldur2x.bdf
 sed -i -e 's/^iso.*-FONT/FONT/g' -e 's/eldur/eldur2x/g' out/eldur2x.bdf
-bnp out/eldur2x.bdf eldur2x otb
+ff eldur2x
 
 rm -f out/*-*.bdf
 
