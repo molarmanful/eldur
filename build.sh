a3c2ababc732fc9e2ca6aec7e0f4da8f7f9419f6
molarmanful <benjinpang@gmail.com>
2024-04-11 12:55:30 -0400

    addressing #2

--- build.sh
+++ build.sh
@@ -28,9 +28,19 @@ ff() {
 	s=$(
 		cat <<-END
 			f = open(argv[1])
+			f.os2_version = 4
+			f.os2_vendor = "BenP"
+			f.os2_panose = (2, 0, 6, 9, 0, 0, 0, 0, 0, 0)
+			f.os2_weight_width_slope_only = True
+			f.selection.all()
+			f.em = 2048
+			f.correctDirection()
+			f.removeOverlap()
+			for g in f:
+			    f[g].width = 768
+			    if f[g].unicode != -1:
+			        f[g].glyphname = nameFromUnicode(f[g].unicode, "AGL with PUA")
 			f.encoding = "UnicodeFull"
-			f.fullname = argv[3]
-			f.fontname = argv[3]
 			f.generate(argv[2], "otb")
 			f.generate(argv[2] + "dfont", "sbit")
 		END
